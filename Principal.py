ca961459abf8d68e0b7d98afe07ee7af903df250
MirandaGonzalo <gonzamirandab2000@gmail.com>
2015-11-27 09:54:14 -0300

    Creamos colisiones entre el actor y la lava, falta terminar

--- Principal.py
+++ Principal.py
@@ -53,6 +53,36 @@ class Principal(pilasengine.actores.Actor):
         
         
     def actualizar(self):
+        """
+        Lava
+        """
+        if self.x>111 and self.x< 343 and self.y <= -180:
+    		print"Muerte"
+    	if self.x>443 and self.x< 525 and self.y <= -180:
+    		print"Tocaste"
+    	if self.x>615 and self.x< 755 and self.y <= -180:
+    		print"Me Queme"
+    	if self.x>945 and self.x< 1170 and self.y <= -180:
+    		print"Que Dolor"
+    	"""
+    	Toxicos
+    	"""
+    	if self.x>-2926 and self.x< -2597 and self.y <= -183:
+    		print"vav"
+    	if self.x>-2166 and self.x< -1916 and self.y <= -151:
+    		print"asdc"
+    	"""
+    	if self.x>111 and self.x< 346 and self.y <= -151.6:
+    		print"qwerty"
+    	if self.x>111 and self.x< 346 and self.y <= -151.6:
+    		print"Hola"
+    	if self.x>111 and self.x< 346 and self.y <= -151.6:
+    		print"Chau"
+    	if self.x>111 and self.x< 346 and self.y <= -151.6:
+    		print"Salvacion"
+    	if self.x>111 and self.x< 346 and self.y <= -151.6:
+    		print"HP"
+  		"""
         velocidad = 10
         salto = 10
         self.x = self.figura.x
@@ -70,11 +100,12 @@ class Principal(pilasengine.actores.Actor):
  
         pilas.camara.x=self.x
 
+"""
 class Golpe(pilasengine.actores.Actor):
 	def iniciar(self):
 		self.imagen="espada.png"
 		personaje.hide()
-
+"""
 
 #Esta clase es la del enemigo
 class Enemigo(pilasengine.actores.Actor):
@@ -98,33 +129,38 @@ class MiMunicion(pilasengine.actores.Actor):
         self.espejado=True
 
 
-
 def victoria(personaje, lanzador):
     intro = pilas.musica.cargar("Hola.mp3")
     intro.reproducir(repetir=True)
     
+def Muere_enemigo(bonus, lanzador):
+	lanzador.eliminar()
+	bonus.eliminar()
+
 
 def tirar():
     lanzador.disparar()
     return True
 
-def Perder():
+def perder():
     global personaje
     global a
     a = a - 1
     if a!=0:
         if a==1:
-            pilas.avisar("Te queda "+str(a)+" vida. ENCONTRA UNA OFERTA RÁPIDO")
+            pilas.avisar("Te queda "+str(a)+" vida. ENCONTRA UNA OFERTA RAPIDO")
         else:
             pilas.avisar("Te quedan "+str(a)+" vidas")
     if (a == 0):
         personaje.eliminar()
         pilas.avisar("Perdiste")
-        
+
 pilas.actores.vincular(MiMunicion)
 pilas.actores.vincular(Principal)
 pilas.actores.vincular(Enemigo)
-
+"""
+bonus = Golpe(pilas)
+"""
 personaje = pilas.actores.Principal()
 personaje.escala_x = .3
 personaje.escala_y = .3
@@ -139,12 +175,16 @@ lanzador.x=150
     
 pilas.colisiones.agregar(personaje, lanzador, victoria)  
 
-lanzador.aprender("Disparar", municion="MiMunicion")
+lanzador.aprender("Disparar", municion="MiMunicion", grupo_enemigos = personaje, cuando_elimina_enemigo = perder)
 
 pilas.tareas.agregar(0.5,tirar)
 
-pilas.colisiones.agregar(lanzador, personaje, Perder)
 
+
+
+"""
+pilas.colisiones.agregar(bonus, lanzador,Muere_enemigo)
+"""
 pilas.comportamientos.vincular(SaltarUnaVez)
 
 pilas.ejecutar()
