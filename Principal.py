7cb60abc62fa261ec0f9a5166777bcb1c82d619a
MirandaGonzalo <gonzamirandab2000@gmail.com>
2015-12-11 11:34:41 -0300

    Unimos los 2 actores en el mismo archivo, creamos la colision entre estos

--- Principal.py
+++ Principal.py
@@ -4,9 +4,29 @@ import pilasengine
 import time
 import Saltar
 
-
 pilas = pilasengine.iniciar()
+
+mapa = pilas.actores.MapaTiled('plataformas.tmx', densidad=0,
+            restitucion=0, friccion=0, amortiguacion=0)
+
+
 pilas.comportamientos.vincular(Saltar.Subir)
+
+#Esta clase es la del enemigo
+class enemigo(pilasengine.actores.Actor):
+    def iniciar(self):
+        self.imagen ="Dios.jpg"
+        self.direccion=-1
+        self.espejado=True
+    def actualizar(self):
+        #Funcion que permite el movimiento de el enemigo
+        if self.x <= -300:
+            self.direccion=1
+            self.espejado = False
+        if self.x >= 300:
+            self.direccion=-1
+            self.espejado = True
+        self.x+=self.direccion * 5  
 
 #Esta clase crea al Actor   
 class Principal(pilasengine.actores.Actor):
@@ -24,8 +44,6 @@ class Principal(pilasengine.actores.Actor):
         self.y = self.figura.y
         if self.pilas.control.derecha:
             self.figura.velocidad_x = velocidad
-            grilla = pilas.imagenes.cargar_grilla("prueba.png", 4)
-            p = pilas.actores.Animacion(grilla, True)
         elif self.pilas.control.izquierda:
             self.figura.velocidad_x = -velocidad
         else:
@@ -34,17 +52,26 @@ class Principal(pilasengine.actores.Actor):
         if self.esta_pisando_el_suelo():
             if self.pilas.control.arriba and int(self.figura.velocidad_y) <= 0:
                 self.figura.impulsar(0, 5)
-                
         self.sensor_pies.x = self.x
         self.sensor_pies.y = self.y - 20
         
     def esta_pisando_el_suelo(self):
         return len(self.sensor_pies.figuras_en_contacto) > 0
+   
+def Vida_menos(lanzador, personaje):
+        personaje.eliminar()
+        pilas.avisar("Perdiste")
     
-mapa = pilas.actores.MapaTiled('plataformas.tmx', densidad=0,
-            restitucion=0, friccion=0, amortiguacion=0)
-        
+lanzador= enemigo(pilas)
+lanzador.escala_x= .7
+lanzador.escala_y= .7
+lanzador.y=50
+lanzador.x=150
+    
 personaje = Principal(pilas)
 personaje.escala_x = .3
 personaje.escala_y = .3
+
+pilas.colisiones.agregar(lanzador, personaje, Vida_menos)
+
 pilas.ejecutar()
